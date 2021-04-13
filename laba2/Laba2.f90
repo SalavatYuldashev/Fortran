bda4b7bdd9f0c218fd7376c02a11044724477569
SalavatYuldashev <salich2@yandex.ru>
2021-04-13 10:51:38 +0300

    Update Laba2.f90
    update laba2

--- laba2/Laba2.f90
+++ laba2/Laba2.f90
@@ -10,14 +10,18 @@ yes = 'y'
 no = 'n'
 
 100 print *, 'Введите x1, x2, x3 соответствующих условию x1<x2<x3'
-read(*,*) x1, x2, x3
+read(*,*) x1, x2, x3, ERR 150
 
   if ((x1 < x2) .and. (x2 <x3)) then
     print *, 'Введите y1, y2, y3 соответствующих x1, x2, x3'
-    read(*,*) y1, y2, y3 ERR 200
+    read(*,*) y1, y2, y3, ERR 200
     print *, 'Введите значние аргумента x в промежутке от ', x1, ' до ', x3
-    read(*,*) x ERR 300
-    piecewiseLinearInterpolation(x)
+    read(*,*) x, ERR 300
+      if ((x >= x1) .and. (x<=x3))
+	    piecewiseLinearInterpolation(x)
+	  else 
+	    goto 300
+	  end if
   else 
     print *, 'Введенные значения x1, x2, x3  не соответствуют условию \"x1<x2<x3\"&
     попробуйте снова'
@@ -36,16 +40,18 @@ real(4) function piecewiseLinearInterpolation(x)
     implicit none
     if((x >= x1) .and. (x <=x2)) then
 	  y = y1 + (x-x1)/(x2-x1)*(y2-y1)
-	  print *, y
-	else ((x >= x2) .and. (x <=x3)) then
+	  print *,'y= ', y
+	elseif ((x >= x2) .and. (x <=x3)) then
 	  y = y2 + (x-x2)/(x3-x2)*(y3-y2)
-	  print *, y
-	else ERR 300
+	  print *,'y= ', y
 	end if
 end function piecewiseLinearInterpolation
 GOTO 400
-
+150 print *, 'Значения x1, x2, x3 заданы не корректно &'
+GOTO 400
 200 print *, 'Значения y1, y2, y3 заданы не корректно &'
+GOTO 400
 300 print *, 'Значениe агрумента x задано не корректно&'
+GOTO 400
 400 print *, 'Программа завершена!'
 end program Laba2
