b07365f8c466ddba608d6e855434da09fd56121b
SalavatYuldashev <salich2@yandex.ru>
2021-04-13 10:21:44 +0300

    Update Laba2.f90
    create new laba2

--- laba2/Laba2.f90
+++ laba2/Laba2.f90
@@ -1,46 +1,51 @@
 program Laba2
 implicit none
 
-100 real(4) :: x1, x2, x3
+real(4) :: x1, x2, x3
 real(4) :: y1, y2, y3
-real(4) :: x, y, midleY1, midleY2
-print *, 'Введите x1, x2, x3 соответствующих условию x1<x2<x3'
-read(*,*) x1, x2, x3
-print *, 'Введите y1, y2, y3 соответствующих x1<x2<x3'
-read(*,*) y1, y2, y3
-if ((x1 < x2) .and. (x2<x3)) then
-  midleY1 = y1 + (x-x1)/(x2-x1) * (y2 - y1)
-  midleY2 = y2 + (x-x2)/(x3-x2) * (y3 - y2)
-else 
-  print *, 'Введенные x1, x2, x3  не соответствуют условию x1<x2<x3&
-   попробуйте снова'
-  goto 100
-end if
+real(4) :: x, y
+character(len=1):: option, yes, no
 
-!real(4) :: summand1=1, summand2, summand3, summand4
-!character (len=7) :: cosX = "cos(x)=" 
-!summand2 = x**2/factorial(2) 
-!summand3 = x**4/factorial(4) 
-!summand4 = x**6/factorial(6) 
-!y = summand1 - summand2 + summand3 - summand4
-!print *, y
-!print *, cos(x)
-!write(*,*) cosx, summand1, '-', summand2, '+', summand3, '-', summand4
-if()
- 
+yes = 'y'
+no = 'n'
+
+100 print *, 'Введите x1, x2, x3 соответствующих условию x1<x2<x3'
+read(*,*) x1, x2, x3
+
+  if ((x1 < x2) .and. (x2 <x3)) then
+    print *, 'Введите y1, y2, y3 соответствующих x1, x2, x3'
+    read(*,*) y1, y2, y3 ERR 200
+    print *, 'Введите значние аргумента x в промежутке от ', x1, ' до ', x3
+    read(*,*) x ERR 300
+    piecewiseLinearInterpolation(x)
+  else 
+    print *, 'Введенные значения x1, x2, x3  не соответствуют условию \"x1<x2<x3\"&
+    попробуйте снова'
+    print *, 'Для повтора введите \"y\", для завершения \"n\"'
+    read(*,*) option ERR 400
+      if (option .eq. yes)then
+        goto 100
+	  else
+	    goto 400
+      end if
+  end if
+  
 contains
 		
-integer(4) function factorial(n) 
-implicit none
+real(4) function piecewiseLinearInterpolation(x) 
+    implicit none
+    if((x >= x1) .and. (x <=x2)) then
+	  y = y1 + (x-x1)/(x2-x1)*(y2-y1)
+	  print *, y
+	else ((x >= x2) .and. (x <=x3)) then
+	  y = y2 + (x-x2)/(x3-x2)*(y3-y2)
+	  print *, y
+	else ERR 300
+	end if
+end function piecewiseLinearInterpolation
+GOTO 400
 
-integer(4) :: n, fact, i
-fact=1
-do i=1, n
-fact=fact*i
-end do
-
-factorial = fact
-
-end function factorial
-	
+200 print *, 'Значения y1, y2, y3 заданы не корректно &'
+300 print *, 'Значениe агрумента x задано не корректно&'
+400 print *, 'Программа завершена!'
 end program Laba2
